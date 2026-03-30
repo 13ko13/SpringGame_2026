847dfbb2c9d55f0ea2ddf5941483827092ec10b5
ko1307 <208754810+13ko13@users.noreply.github.com>
2026-03-30 23:44:23 +0900

    規模が小さい当たり判定管理クラスを作成

---
+++ SpringGame_2026/SpringGame_2026/System/CollisionManager.cpp
@@ -0,0 +1,32 @@
+﻿#include "CollisionManager.h"
+#include "../GameObjects/Player.h"
+#include "../Math/Sphere.h"
+#include "../GameObjects/Enemy.h"
+
+CollisionManager::CollisionManager()
+{
+}
+
+CollisionManager::~CollisionManager()
+{
+}
+
+void CollisionManager::Update(std::shared_ptr<Player>& pPlayer, std::shared_ptr<Enemy>& pEnemy)
+{
+	//攻撃中のみ当たり判定を行う
+	if (pPlayer->IsAttacking())
+	{
+		if (CheckCollision(pPlayer->GetAttackSphere(), pEnemy))
+		{
+			//敵に当たっているときの処理を行わせる
+			pEnemy->OnHit();
+			pPlayer->OnAttackedEnemy();
+		}
+	}
+}
+
+bool CollisionManager::CheckCollision(const Sphere& attackSphere,
+		std::shared_ptr<Enemy>& pEnemy)
+{
+	return attackSphere.HitCollision(pEnemy->GetSphere());
+}
---
+++ SpringGame_2026/SpringGame_2026/System/CollisionManager.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include <memory>
+
+//前方宣言
+class Player;
+class Enemy;
+class Sphere;
+
+class CollisionManager
+{
+public:
+	CollisionManager();
+	~CollisionManager();
+
+	void Update(
+		std::shared_ptr<Player>& pPlayer,
+		std::shared_ptr<Enemy>& pEnemy
+	);
+
+private:
+	/// <summary>
+	/// プレイヤーの攻撃判定球と敵の当たり判定球の衝突判定
+	/// </summary>
+	/// <param name="attackSphere">プレイヤーの攻撃判定用の球</param>
+	/// <param name="pEnemy">敵の当たり判定用の球</param>
+	/// <returns>true:あたっている,false:当たっていない</returns>
+	bool CheckCollision(const Sphere& attackSphere,
+		std::shared_ptr<Enemy>& pEnemy);
+};
+
