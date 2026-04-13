622cfe3d862bb226161a8108ef1d135c5cf21b8e
13ko13 <fko2547008@stu.o-hara.ac.jp>
2026-04-13 18:58:06 +0900

    Fadeクラスを作成してFadeを行えるようにした

---
+++ SpringGame_2026/SpringGame_2026/Manager/Fade.cpp
@@ -0,0 +1,109 @@
+﻿#include "Fade.h"
+#include "../Main/Application.h"
+#include <DxLib.h>
+
+Fade::Fade()
+{
+}
+
+Fade::~Fade()
+{
+}
+
+void Fade::Update()
+{
+	//状態によってアルファ値を更新する
+	switch (m_currentState)
+	{
+	case Fade::FadeState::FadeIn:
+		m_currentFrame--;
+		if (m_currentFrame <= 0)
+		{
+			m_currentState = FadeState::None;
+			m_isFading = false;
+		}
+		break;
+	case Fade::FadeState::FadeOut:
+		m_currentFrame++;
+		if (m_currentFrame >= m_fadeFrame)
+		{
+			m_currentState = FadeState::None;
+			m_isFading = false;
+		}
+		break;
+	case Fade::FadeState::None:
+		break;
+	default:
+		break;
+	}
+}
+
+void Fade::Draw()
+{
+	//アルファ値をもとにフェード用の四角形を描画する
+	switch (m_currentState)
+	{
+	case Fade::FadeState::FadeIn:
+	{
+		//ウィンドウに黒い四角形を描画するためにウィンドウサイズを取得する
+		const Size& windowSize = Application::GetInstance().GetWindowSize();
+		//フェードのアルファ割合を計算する
+		m_alpha = m_currentFrame / m_fadeFrame;
+
+		//アルファ値をもとにウィンドウ全体に黒い四角形を描画する
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, static_cast<int>(m_alpha * 255));
+		//黒い四角形描画する
+		DrawBox(0, 0, windowSize.w, windowSize.h, GetColor(0, 0, 0), TRUE);
+		//描画後はブレンドモードを元に戻す
+		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+		break;
+	}
+	case Fade::FadeState::FadeOut:
+	{
+		//ウィンドウに黒い四角形を描画するためにウィンドウサイズを取得する
+		const Size& windowSize = Application::GetInstance().GetWindowSize();
+		//フェードのアルファ割合を計算する
+		m_alpha = 1.0f - static_cast<float>(m_currentFrame) / m_fadeFrame;
+
+		//アルファ値をもとにウィンドウ全体に黒い四角形を描画する
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, static_cast<int>((1.0f - m_alpha) * 255));
+		//黒い四角形描画する
+		DrawBox(0, 0, windowSize.w, windowSize.h, GetColor(0, 0, 0), TRUE);
+		//描画後はブレンドモードを元に戻す
+		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+		break;
+	}
+	case Fade::FadeState::None:
+		break;
+	default:
+		break;
+	}
+}
+
+void Fade::StartFadeIn(float fadeFrame)
+{
+	//フェード中なら処理を飛ばす
+	if (m_isFading) return;
+
+	//フェードインの状態にする
+	m_currentState = FadeState::FadeIn;
+	//フェードにかけるフレーム数をセットする
+	m_currentFrame = fadeFrame;
+	m_fadeFrame = fadeFrame;
+	//フェード中フラグを立てる
+	m_isFading = true;
+}
+
+void Fade::StartFadeOut(float fadeFrame)
+{
+	//フェード中なら処理を飛ばす
+	if (m_isFading) return;
+
+	//フェードアウトの状態にする
+	m_currentState = FadeState::FadeOut;
+	//フェードにかけるフレーム数をセットする
+	m_fadeFrame = fadeFrame;
+	m_currentFrame = 0;
+	//フェード中フラグを立てる
+	m_isFading = true;
+}
---
+++ SpringGame_2026/SpringGame_2026/Manager/Fade.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+
+class Fade
+{
+public:
+	Fade();
+	~Fade();
+
+	void Update();
+	void Draw();
+
+	/// <summary>
+	/// 指定フレーム数かけてフェードインする
+	/// </summary>
+	/// <param name="fadeFrame">フェードにかけるフレーム数</param>
+	void StartFadeIn(float fadeFrame);
+
+	/// <summary>
+	/// 指定フレーム数かけてフェードアウトする
+	/// </summary>
+	/// <param name="fadeFrame">フェードにかけるフレーム数</param>
+	void StartFadeOut(float fadeFrame);
+
+	/// <summary>
+	/// フェード中かどうかを取得する
+	/// </summary>
+	/// <returns>フェード中であればtrue、そうでなければfalse</returns>
+	const bool IsFading() const {return m_isFading;}
+
+	/// <summary>
+	/// フェードにかけるフレーム数を取得する
+	/// </summary>
+	/// <returns>フェードにかけるフレーム数</returns>
+	const float GetFadeFrame() const { return m_fadeFrame; }
+
+private:
+	//フェードの状態
+	enum class FadeState
+	{
+		FadeIn,		//フェードイン中
+		FadeOut,	//フェードアウト中
+		None,		//フェードなし
+	};
+
+	FadeState m_currentState = FadeState::None;	//現在のフェードの状態
+
+	float m_alpha = 0;				//フェードのアルファ値	
+	float m_fadeFrame = 0;			//フェードにかけるフレーム数
+	float m_currentFrame = 0;		//現在のフレーム数
+
+	//フェード中か
+	bool m_isFading = false;
+};
+
